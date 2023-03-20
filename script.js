1626afeb3f71e2986d0d84ebe830378a57776da0
lancast <angelprep92@gmail.com>
2023-03-20 12:37:18 -0600

    Quinta clase del cursos de manipulacion del DOM con JS, web de tareas

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+( () => { 
+
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
@@ -7,20 +9,50 @@ const createTask = (evento) => {
     const list = document.querySelector('[data-list]');
     const task = document.createElement('li');
     task.classList.add('card');
-    input.value = ""; 
-    const content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
-        <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+    input.value = ''; 
 
+    ;
+    const taskContent = document.createElement('div');
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value; 
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+   
+
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
-
-    console.log(content);
 };
 
-console.log(btn);
-
-
 btn.addEventListener('click', createTask);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener("click", completeTask);  
+    return i;
+};
+
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+};
+
+const deleteIcon = () => {
+    const i = document.createElement("i");
+    i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+    i.addEventListener('click', deleteTask);  
+    return i;
+};
+
+const deleteTask = (event) => {
+    const parent = event.target.parentElement;
+    parent.remove();
+
+
+};
+
+})();
