dccc9927b21065970a1f4edb4d311cdb9f066786
lancast <angelprep92@gmail.com>
2023-03-20 09:28:04 -0600

    Tercera clase del cursos de manipulacion del DOM con JS

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const btn = document.querySelector('[data-form-btn]');
-const input = document.querySelector('[data-form-input]');
+
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    console.log(input.value);
+};
 
 console.log(btn);
 
 
-btn.addEventListener('click', function() {
-    const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
-});
+btn.addEventListener('click', createTask);
